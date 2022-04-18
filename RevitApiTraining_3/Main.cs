db66bf7069abb70a9261fcc6e37526dec8b63ef3
vermei2@outlook.com <vermei2@outlook.com>
2022-04-18 20:35:38 +0300

    Добавьте файлы проекта.

---
+++ RevitApiTraining_3/Main.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RevitApiTraining_3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedRef = uidoc.Selection.PickObject(ObjectType.Element, "Выберите элемент");
+
+            var selectedElement = doc.GetElement(selectedRef);
+            if (selectedElement is Wall)
+            {
+                Parameter lengthParameter1 = selectedElement.LookupParameter("Длина");
+                if (lengthParameter1.StorageType==StorageType.Double)
+                {
+                    TaskDialog.Show("Length1", lengthParameter1.AsDouble().ToString());
+                }
+                Parameter lengthParameter2 = selectedElement.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH);
+                if (lengthParameter2.StorageType == StorageType.Double)
+                {
+                    TaskDialog.Show("Length2", lengthParameter2.AsDouble().ToString());
+                }
+            }
+
+
+            return Result.Succeeded;
+        }
+    }
+}
